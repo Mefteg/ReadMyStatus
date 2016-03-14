acafaf2d80d5e7ff8024949d6eee718577a5dd87
Mefteg <gimenez.tom@gmail.com>
2016-03-14 07:46:31 -0400

    Send disk info on pin 11

--- Arduino/ReadMyStatus/ReadMyStatus.ino
+++ Arduino/ReadMyStatus/ReadMyStatus.ino
@@ -17,10 +17,12 @@ Ask for my tablet status periodically.
 #define DELAY_AFTER_UPDATE 1000
 
 #define PIN_BATTERY 10
+#define PIN_DISK 11
 
 TheAirBoard board;
 
-float battery = 0.0f;
+unsigned int battery = 0;
+unsigned int disk = 0;
 
 // the setup routine runs once when you press reset:
 void setup()
@@ -33,6 +35,7 @@ void setup()
   analogWrite(BLUE, LOW);
 
   pinMode(PIN_BATTERY, OUTPUT);
+  pinMode(PIN_DISK, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
@@ -41,7 +44,9 @@ void loop()
   // check the battery
   board.batteryChk();
 
-  analogWrite(PIN_BATTERY, 255.0f * battery);
+  // update display
+  analogWrite(PIN_BATTERY, (unsigned int) (255.0f * (battery * 0.01f)));
+  analogWrite(PIN_DISK, (unsigned int) (255.0f * (disk * 0.01f)));
   
 	// if something is available to read
 	if(Serial.available())
@@ -63,13 +68,35 @@ void loop()
     		int indexDiskSeparator = data.indexOf(TAG_DISK_SEPARATOR, indexBatterySeparator);
 	    	if (indexDiskSeparator > -1)
 	    	{
-	    		dataDisk = data.substring(indexBatterySeparator, indexDiskSeparator);
+	    		dataDisk = data.substring(indexBatterySeparator + 1, indexDiskSeparator);
 	    	} 
     	}
 
 		// if all data are OK
     	if (dataBattery.length() > 0 && dataDisk.length() > 0)
     	{
+        // update values
+        battery = dataBattery.toInt();
+        if (battery < 0)
+        {
+          battery = 0;
+        }
+        if (battery > 100)
+        {
+          battery = 100;
+        }
+
+        disk = dataDisk.toInt();
+        if (disk < 0)
+        {
+          disk = 0;
+        }
+        if (disk > 100)
+        {
+          disk = 100;
+        }
+
+        // send message success
     		Serial.write(MESSAGE_TYPE_SUCCESS);
     		delay(DELAY_AFTER_UPDATE);
     	}
@@ -88,3 +115,4 @@ void loop()
     // wait a bit
     delay(DELAY_BEFORE_UPDATE);
 }
+
